c5f08b936c24c3d8bb9588488d4dfde3740f39c1
ootakito <165157559+ootakito@users.noreply.github.com>
2024-04-17 19:37:10 +0900

    コミット

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,13 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Nagomi170c
+module Nagomi_170c
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_storage.variant_processor = :mini_magick
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,5 +20,11 @@ module Nagomi170c
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
--- db/migrate/20240417091329_create_prototypes.rb
+++ db/migrate/20240417091329_create_prototypes.rb
@@ -1,7 +1,10 @@
 class CreatePrototypes < ActiveRecord::Migration[7.0]
   def change
     create_table :prototypes do |t|
-
+      t.string :title, null: false
+      t.text :catch_copy, null: false
+      t.text :concept, null: false
+      #t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
