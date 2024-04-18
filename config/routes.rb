e4ee102528ac606adad331d659088e5ce88a6484
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-18 15:33:46 +0900

    prototypesコントローラーからの全てのアクションを許可

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "prototypes#index"
-  resources :prototypes, only: [:index, :new, :create] do
+  resources :prototypes do
     resources :comments, only: :create
   end
 end
