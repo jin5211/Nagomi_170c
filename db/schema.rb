ee9ba378ba8421454871118bcce8ceb282ed66ad
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-18 17:09:52 +0900

    データベース構造のconflict解消

--- db/schema.rb
+++ db/schema.rb
@@ -55,6 +55,22 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_061721) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.text "name", null: false
+    t.text "profile", null: false
+    t.text "occupation", null: false
+    t.text "position", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
