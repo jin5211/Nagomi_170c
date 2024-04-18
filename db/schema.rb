8ada5e97e99030ecdb6811921caa83290541dcf8
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-18 19:58:45 +0900

    commentsテーブルによるconflict解消

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_04_18_102649) do
-  create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
+  create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_102649) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", charset: "utf8mb4", force: :cascade do |t|
+  create_table "active_storage_blobs", charset: "utf8", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,7 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_102649) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", charset: "utf8mb4", force: :cascade do |t|
+  create_table "active_storage_variant_records", charset: "utf8", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
@@ -47,15 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_102649) do
     t.index ["prototype_id"], name: "index_comments_on_prototype_id"
   end
 
-  create_table "comments", charset: "utf8", force: :cascade do |t|
-    t.string "content", null: false
-    t.bigint "prototype_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["prototype_id"], name: "index_comments_on_prototype_id"
-  end
-
-  create_table "prototypes", charset: "utf8mb4", force: :cascade do |t|
+  create_table "prototypes", charset: "utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "catch_copy", null: false
     t.text "concept", null: false
@@ -65,7 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_102649) do
     t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.text "name", null: false
     t.text "profile", null: false
     t.text "occupation", null: false
