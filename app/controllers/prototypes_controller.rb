8f4217a555643fc18b5e69fdca5257b826cbbec3
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-18 14:35:51 +0900

    imageも受け取るように設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -9,11 +9,14 @@ class PrototypesController < ApplicationController
 
   def create
     @prototype = Prototype.new(prototype_params)
+    @prototype.valid?
+    @prototype.save
+    redirect_to '/'
   end
 
   private
 
   def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept)
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
   end
 end
