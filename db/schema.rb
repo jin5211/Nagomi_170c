7c5bf669d901caa109c20a79ad48ba371ed74d66
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-18 20:11:13 +0900

    commentsテーブルにuser追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_18_102649) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_18_110038) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -44,7 +44,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_102649) do
     t.bigint "prototype_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["prototype_id"], name: "index_comments_on_prototype_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "prototypes", charset: "utf8", force: :cascade do |t|
@@ -75,5 +77,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_102649) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "users"
   add_foreign_key "prototypes", "users"
 end
