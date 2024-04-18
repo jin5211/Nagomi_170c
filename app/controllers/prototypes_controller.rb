8408b5f8c731f2397a3b28c783560af7277c18f8
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-18 14:27:27 +0900

    https://github.com/jin5211/Nagomi_170c/pull/14#discussion_r1569720740 permitを適切に行えるよう修正しました。

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -14,6 +14,6 @@ class PrototypesController < ApplicationController
   private
 
   def prototype_params
-    params.require(:prototype).permitted(:title, :catch_copy, :concept)
+    params.require(:prototype).permit(:title, :catch_copy, :concept)
   end
 end
